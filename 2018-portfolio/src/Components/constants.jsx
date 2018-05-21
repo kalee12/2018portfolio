66eb2df98ca5d0e858d2438f8eb52c1164f60771
Karen Lee <leekaren888@gmail.com>
2018-05-20 23:57:19 -0700

    Updates

--- 2018-portfolio/src/Components/constants.jsx
+++
@@ -1,9 +0,0 @@
-export default {
-    routes: {
-        main: "/",
-        projects: "/projects",
-        photography: "/photography",
-        about: "/about",
-        project: "/:project"
-    }
-}
